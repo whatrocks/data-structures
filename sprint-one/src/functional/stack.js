484175d9ef3511f7a9707f8f4563b969ac9c1bc8
Charles Harrington <charles.harrington@gmail.com>
2015-10-07 12:00:01 -0700

    Reports size of 0 for a new stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -4,6 +4,8 @@ var Stack = function() {
   // Use an object with numeric keys to store values
   var storage = {};
 
+  var length = 0;
+
   // Implement the methods below
   someInstance.push = function(value) {
   };
@@ -12,6 +14,7 @@ var Stack = function() {
   };
 
   someInstance.size = function() {
+    return length;
   };
 
   return someInstance;
