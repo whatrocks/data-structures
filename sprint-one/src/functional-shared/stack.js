f746c585594fd80e7160a89180df803989075df6
whatrocks <charles.harrington@gmail.com>
2015-10-07 14:31:51 -0700

    Functional-shared Stack: Returns length of 0 for newly created Stack

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,29 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var someInstance = {};
+  someInstance.storage = {};
+  someInstance.length = 0;
+  _.extend(someInstance, stackMethods);
+  return someInstance;
+
 };
 
-var stackMethods = {};
+var stackMethods = {
+
+  push: function(value) {
+
+  },
+
+  pop: function() {
+
+  },
+
+  size: function() {
+    return this.length;
+  }
+
+};
 
 
