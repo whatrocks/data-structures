b771cf8917887d676d39006c4aeb825326f3f088
whatrocks <charles.harrington@gmail.com>
2015-10-07 15:16:03 -0700

    prototypal stack: add stack

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -9,13 +9,17 @@ var Stack = function() {
 
 var stackMethods = {
   push: function(value){
+    this.storage[this.length] = value;
     this.length++;
   },
 
   pop: function(){
     if(this.length > 0){
+      var result = this.storage[this.length - 1];
+      delete this.storage[this.length - 1];
       this.length--;
     }
+    return result;
   },
 
   size: function() {
