39717bbc3cbbe0cbe6865c3fdea494c2dc3192be
Charles Harrington <charles.harrington@gmail.com>
2015-10-07 12:13:15 -0700

    Reports length of 0 for new queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -4,6 +4,8 @@ var Queue = function() {
   // Use an object with numeric keys to store values
   var storage = {};
 
+  var length = 0;
+
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
@@ -13,6 +15,7 @@ var Queue = function() {
   };
 
   someInstance.size = function() {
+    return length;
   };
 
   return someInstance;
