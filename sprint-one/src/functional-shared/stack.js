b2788f5bf9a27d8fe763e095443b61348293a77d
whatrocks <charles.harrington@gmail.com>
2015-10-07 14:48:36 -0700

    Function-shared Queue: Pushes and pops into the storage variable

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -13,14 +13,19 @@ var Stack = function() {
 var stackMethods = {
 
   push: function(value) {
+    this.storage[this.length] = value; 
     this.length++;
+    return value;
   },
 
   pop: function() {
-    if ( this.length > 0 ) {
-      this.length--;
-    }
 
+    if ( this.length > 0 ) {
+      var result = this.storage[this.length - 1];
+      delete this.storage[this.length - 1];
+      this.length--;
+      return result;
+    }
   },
 
   size: function() {
