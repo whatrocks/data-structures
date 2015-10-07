2db6224f281d19f630dac7d9dea6a5fc83fd2026
whatrocks <charles.harrington@gmail.com>
2015-10-07 15:14:02 -0700

    prototype stack: set up with length

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,26 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(stackMethods);
+  someInstance.storage = {};
+  someInstance.length = 0;
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value){
+    this.length++;
+  },
+
+  pop: function(){
+    if(this.length > 0){
+      this.length--;
+    }
+  },
+
+  size: function() {
+    return this.length;
+  }
+};
 
 
