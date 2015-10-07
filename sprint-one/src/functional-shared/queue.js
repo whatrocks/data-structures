d352466704f0749a7f3d99c1298006124fbc8b48
whatrocks <charles.harrington@gmail.com>
2015-10-07 14:54:27 -0700

    Functional-shared queue: Add length structure

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,26 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.storage = {};
+  someInstance.length = 0;
+  _.extend(someInstance, queueMethods);
+
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value){
+    this.length++;
+  },
+  dequeue: function(){
+    if (this.length > 0){
+      this.length--;
+    }
+  },
+  size: function() {
+    return this.length;    
+  }
+};
 
 
