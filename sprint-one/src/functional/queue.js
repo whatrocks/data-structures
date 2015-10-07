ed240cff9fb3b0cd5ef798255dc50de7c2f9b00b
Charles Harrington <charles.harrington@gmail.com>
2015-10-07 12:24:08 -0700

    Queue: deueue will report first item and move rest of items in storage accordingly

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -11,12 +11,27 @@ var Queue = function() {
 
   someInstance.enqueue = function(value) {
     length++;
+    storage[length] = value;
   };
 
   someInstance.dequeue = function() {
+    var result;
     if ( length > 0 ) {
+
+      // first item goes into result
+      result = storage[1];
+      // delete the first item
+      delete storage[1];
+
+      // Move everything down an index
+      for (var i = 1; i < length; i++ ) {
+        storage[i] = storage[i + 1];
+      }
+      
+      // reduce the total length
       length--;
     }
+    return result;
   };
 
   someInstance.size = function() {
