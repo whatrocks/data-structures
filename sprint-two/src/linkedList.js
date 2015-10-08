be4c53db3262c2b5e1b79629328e33d39daad4fe
whatrocks <charles.harrington@gmail.com>
2015-10-08 10:23:31 -0700

    Implemented LinkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,47 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+
+    // change list.tail to the value of the new node
+    var newNode = Node(value);
+    if ( list.tail ) {
+      list.tail.next = newNode;    
+    }
+    list.tail = newNode;
+    
+    if (list.head === null) {
+      list.head = newNode;
+    }
+
   };
 
   list.removeHead = function() {
+
+    if ( list.head ) {
+      var formerHead = list.head.value;
+      if ( list.head.next) {
+        list.head = list.head.next;
+      } else {
+        list.head = null;
+      }
+      return formerHead;
+    }
   };
 
   list.contains = function(target) {
+
+    var currentNode = list.head;
+
+    while ( currentNode.next !== null ) {
+
+      if ( currentNode.value === target ) {
+        break;
+      } else {
+        currentNode = currentNode.next;
+      }
+    }
+    return currentNode.value === target;
+
   };
 
   return list;
