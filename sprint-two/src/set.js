f6ec196f28b2584abce01cf6084ee6e6482baefe
whatrocks <charles.harrington@gmail.com>
2015-10-08 15:17:16 -0700

    add set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,21 +1,28 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
-  set._storage = undefined;
+  if (!(item in this._storage)) {
+    this._storage[item] = item;
+  }
 };
 
 setPrototype.contains = function(item) {
+  return this._storage[item] === item ? true : false;
 };
 
 setPrototype.remove = function(item) {
+  delete this._storage[item];
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+  setPrototype.add: O(1)
+  setPrototype.contains: O(1)
+  setPrototype.remove: O(1)
  */
