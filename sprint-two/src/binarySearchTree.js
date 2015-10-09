ce619d14ae2efadaa601b6789e6d5b19a61474ba
whatrocks <charles.harrington@gmail.com>
2015-10-09 12:25:47 -0700

    Implement "binary search tree" and pass tests

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,71 @@
 var BinarySearchTree = function(value) {
+  var BST = Object.create(BinarySearchTree.prototype);
+  BST.value = value;
+  BST.left = null;
+  BST.right = null;
+  return BST;
 };
 
+BinarySearchTree.prototype.insert = function (value) {
+  var tree = BinarySearchTree(value);
+  var findPosition = function (node) {
+    if (tree.value < node.value) {
+      if (node.left === null) {
+        node.left = tree;
+      } else {
+        findPosition(node.left);
+      }
+    } else {
+      if (node.right === null) {
+        node.right = tree;
+      } else {
+        findPosition(node.right);
+      }
+    }
+  };
+  findPosition(this);
+};
+
+BinarySearchTree.prototype.contains = function (value){
+  var res = false;
+  var search = function (node) {
+    // base case: not found
+    if (node === null) {
+      return;
+    }
+    // value found 
+    if (node.value === value) {
+      res = true;
+      return;
+    }
+    // keep searching 
+    else {
+      if (node.value < value) {
+        search(node.right);
+      } else {
+        search(node.left);
+      }
+    }
+  }
+  search(this);
+  return res;
+};
+
+BinarySearchTree.prototype.depthFirstLog = function (cb){
+  var iterate = function(node){
+    if (node === null) {
+      return;
+    }
+    cb(node.value);
+    iterate(node.left);
+    iterate(node.right);
+  }
+  iterate(this);
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
+  insert: O(log n)
+  contains: O(log n)
+  depthFirstLog: O(n)
  */
