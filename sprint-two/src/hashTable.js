8ae9989872283e4f88d7f0bff7a1386a78a0ad90
whatrocks <charles.harrington@gmail.com>
2015-10-09 11:36:02 -0700

    Implement "hash table" and passed tests

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,24 +7,59 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage[index] = v;
+
+  if ( this._storage[index] === undefined ){
+    this._storage[index] = [];
+  }
+
+  var found = false;
+
+  for ( var i = 0; i < this._storage[index].length; i++ ){
+
+    if ( this._storage[index][i][0] === k ) {
+      found = true;
+      this._storage[index][i][1] = v;
+    }
+  }
+
+  if ( !found ) {
+    var tuple = [k, v];
+    this._storage[index].push(tuple);    
+  }
+
 };
 
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage[index];
+  var bucket = this._storage[index];
+  for ( var i = 0; i < bucket.length; i++ ){
+    if ( bucket[i][0] === k ) {
+      return bucket[i][1];
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage[index] = null;
+  var bucket = this._storage[index];
+
+  for ( var i = 0; i < bucket.length; i++ ) {
+    if ( bucket[i][0] === k ){
+      bucket[i][1] = null;
+    }
+  }
+
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ Insert: Average O(1), Worst O(n)
+ Retrieve: Average O(1), Worse O(n)
+ Remove: Average O(1), Worse O(n)
+
  */
 
 
