0d8584c950c713c346585a01010f28fc4d097476
whatrocks <charles.harrington@gmail.com>
2015-10-10 16:41:13 -0700

    Modify BST tests for new BreadthFirstSearch method

--- sprint-two/spec/binarySearchTreeSpec.js
+++ sprint-two/spec/binarySearchTreeSpec.js
@@ -33,7 +33,23 @@ describe('binarySearchTree', function() {
     var func = function(value){ array.push(value); };
     binarySearchTree.insert(2);
     binarySearchTree.insert(3);
+    binarySearchTree.insert(1);
+    binarySearchTree.insert(6);
     binarySearchTree.depthFirstLog(func);
-    expect(array).to.eql([5,2,3]);
+    expect(array).to.eql([5,2,1,3,6]);
   });
+
+  it('should execute a callback on every value in a tree using "breadthFirstLog"', function(){
+    var array = [];
+    var func = function(value){ array.push(value); };
+    binarySearchTree.insert(2);
+    binarySearchTree.insert(3);
+    binarySearchTree.insert(1);
+    binarySearchTree.insert(6);
+    binarySearchTree.breadthFirstLog(func);
+    console.log(array);
+    expect(array).to.eql([5,2,6,1,3]);
+  });  
+
+
 });
